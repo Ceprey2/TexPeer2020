bf759637c34b90c21b6226032496c89d44de9396
Sergiy Fokin <sergiyborysovych@ukr.net>
2019-12-29 14:14:04 +0200

    Initial commit

--- src/Corpus.java
+++ src/Corpus.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 
 public class Corpus {
 
+    private final ArrayList textsList = null;
+
     private final ArrayList subjectsList = null;
     private final ArrayList styleList = null;
     private final ArrayList genreList = null;
-
 
     private int getCorpusSize()
     {
--- src/Text.java
+++ src/Text.java
@@ -1,8 +1,16 @@
+import java.util.TreeMap;
+
 public class Text {
 
     private String subject;
     private String style;
     private String genre;
+    private String [] wordsList;
+    private String [] stemssList;
+    private String [] uniqueWordsList;
+    private String [] uniqueStemssList;
+    private TreeMap<String, Integer> wordsFrequency;
+    private TreeMap<String, Integer> stemsFrequency;
 
 
     private int getTextSizeInCharacters (){
@@ -22,3 +30,15 @@ public class Text {
         return foundExamples;
     }
 }
+
+//echo "# TexPeer2020" >> README.md
+//        git init
+//        git add README.md
+//        git commit -m "first commit"
+//        git remote add origin https://github.com/Ceprey2/TexPeer2020.git
+//        git push -u origin master
+
+//…or push an existing repository from the command line
+//
+//        git remote add origin https://github.com/Ceprey2/TexPeer2020.git
+//        git push -u origin master
