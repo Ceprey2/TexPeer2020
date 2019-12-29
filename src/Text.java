130ecd988d482271b057d42832df2025930a5847
Sergiy Fokin <sergiyborysovych@ukr.net>
2019-12-29 15:55:25 +0200

    ClassSampleAdded

--- src/Text.java
+++ src/Text.java
@@ -9,8 +9,8 @@ public class Text {
     private String [] stemssList;
     private String [] uniqueWordsList;
     private String [] uniqueStemssList;
-    private TreeMap<String, Integer> wordsFrequency;
-    private TreeMap<String, Integer> stemsFrequency;
+    private TreeMap <String, Integer> wordsFrequency;
+    private TreeMap <String, Integer> stemsFrequency;
 
 
     private int getTextSizeInCharacters (){
@@ -30,15 +30,3 @@ public class Text {
         return foundSamples;
     }
 }
-
-//echo "# TexPeer2020" >> README.md
-//        git init
-//        git add README.md
-//        git commit -m "first commit"
-//        git remote add origin https://github.com/Ceprey2/TexPeer2020.git
-//        git push -u origin master
-
-//…or push an existing repository from the command line
-//
-//        git remote add origin https://github.com/Ceprey2/TexPeer2020.git
-//        git push -u origin master
